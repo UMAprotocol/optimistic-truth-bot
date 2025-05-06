76b50bb69e315ff7dfdb4574c5fedc6291150317
andy8052 <andychorlian@gmail.com>
2025-05-06 12:38:35 -0400

    simplify

--- tweet_generator/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-LLM Oracle Tweet Generator
-
-This package generates and posts tweets about LLM Oracle predictions and outcomes.
-"""
-
-__version__ = '0.1.0'
--- tweet_generator/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Tests for the LLM Oracle Tweet Generator
-"""
